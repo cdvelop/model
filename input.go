4e871f22822c718503b9d63bfe14d99c6b0f8d49
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-05-17 10:44:47 -0400

    rename methods Input struct

--- input.go
+++ input.go
@@ -8,9 +8,9 @@ type Input struct {
 }
 
 type Build interface {
-	Build(id, field_name string, skip_completion_allowed bool) string //construir vista usuario input
+	HtmlTAG(id, field_name string, skip_completion_allowed bool) string //construir vista usuario input
 }
 
 type Validate interface {
-	Validate(data_in string, skip_validation bool) bool //como sera validado
+	DataField(data_in string, skip_validation bool) bool //como sera validado
 }
