802531a4c58eb3bf7ea72a22ff17c1d5c7da4075
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-05-17 11:39:24 -0400

    remove map operations allow in Object

--- object.go
+++ object.go
@@ -4,6 +4,4 @@ type Object struct {
 	Name           string   //nombre del objeto o tabla
 	TextFieldNames []string //nombre de campos mas representativos del objeto o tabla ej: nombre, apellido
 	Fields         []Field  //campos del objeto
-	//operaciones permitidas según nivel de acceso ej: 0,1,2,3,4 + Create bool, Update bool, Delete bool
-	OperationsAllowed map[uint8]Permission
 }
