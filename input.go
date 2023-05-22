32f32ab41b2e4a2a895e7bc8bdea524b79d58239
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-05-22 13:59:05 -0400

    change Build to HtmlTag in Input

--- input.go
+++ input.go
@@ -3,18 +3,18 @@ package model
 type Input struct {
 	Component
 
-	Build
+	HtmlTag
 	Validate
 
 	TestData
 }
 
-type Build interface {
-	HtmlTAG(id, field_name string, allow_skip_completed bool) string //construir vista usuario input
+type HtmlTag interface {
+	HtmlTag(id, field_name string, allow_skip_completed bool) string
 }
 
 type Validate interface {
-	ValidateField(data_in string, skip_validation bool) bool //como sera validado
+	ValidateField(data_in string, skip_validation bool) bool
 }
 
 type TestData interface {
