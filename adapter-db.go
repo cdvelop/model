0abf1736e02c404c98e65661fff7768e207e0617
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-12-14 22:16:39 -0300

    refactor func ReadSyncDataDB

--- adapter-db.go
+++ adapter-db.go
@@ -5,14 +5,15 @@ type IdHandler interface {
 }
 
 type ReadParams struct {
-	FROM_TABLE      string
+	FROM_TABLE      string   //ej: "users,products" or: public.reservation, public.patient"
 	ID              string   // unique search
-	WHERE           []string //
-	SEARCH_ARGUMENT string
-	ORDER_BY        string
-	SORT_DESC       bool //default ASC
-	RETURN_ANY      bool // default string return []map[string]string, any = []map[string]interface{}
-
+	SELECT          string   // "name, phone, address" default *
+	WHERE           []string //"patient.id_patient = reservation.id_patient AND reservation.id_staff = '2'"
+	SEARCH_ARGUMENT string   //"1,4,33"
+	ORDER_BY        string   // name,phone,address
+	SORT_DESC       bool     //default ASC
+	LIMIT           int      // 10, 5, 100. note: Postgres y MySQL: "LIMIT 10", SQLite: "LIMIT 10 OFFSET 0" OR "" no limit
+	RETURN_ANY      bool     // default string return []map[string]string, any = []map[string]interface{}
 }
 
 type ReadResult struct {
@@ -27,17 +28,7 @@ type DataBaseAdapter interface {
 	// items support in server db: []map[string]string, map[string]string
 	CreateObjectsInDB(table_name string, backup_required bool, items any) (err string)
 
-	// from_tables ej: "users,products" or: public.reservation, public.patient"
-	// params: map[string]string ej:{
-	// LIMIT: 10, 5, 100. note: Postgres y MySQL: "LIMIT 10", SQLite: "LIMIT 10 OFFSET 0" OR "" no limit
-	// ORDER_BY: name,phone,address
-	// SELECT: "name, phone, address" default *
-	// WHERE: "patient.id_patient = reservation.id_patient AND reservation.id_staff = '2'"
-	// ARGS: "1,4,33"
-	// }
-	ReadSyncDataDB(from_tables string, params ...map[string]string) (result []map[string]string, err string)
-	//params: callback fun ej: fun([]map[string]string,error)
-	// "ORDER_BY": "patient_name", "SORT":"DESC" DEFAULT "ASC"
+	ReadSyncDataDB(p ReadParams) (result []map[string]string, err string)
 	ReadAsyncDataDB(p ReadParams, callback func(r ReadResult))
 
 	UpdateObjectsInDB(table_name string, data ...map[string]string) (err string)
@@ -45,6 +36,6 @@ type DataBaseAdapter interface {
 
 	CreateTablesInDB(objects []*Object, result func(err string))
 
-	BackupDataBase(callback func(err string))
 	ClearAllTableDataInDB(tables ...string) (err string)
+	BackupDataBase(callback func(err string))
 }
