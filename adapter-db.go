f509aacf0d4129bf3b995d2fb3fc0b73d18b81b8
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-12-15 00:39:37 -0300

    refactor read func and struct db

--- adapter-db.go
+++ adapter-db.go
@@ -5,21 +5,25 @@ type IdHandler interface {
 }
 
 type ReadParams struct {
-	FROM_TABLE      string   //ej: "users,products" or: public.reservation, public.patient"
-	ID              string   // unique search
-	SELECT          string   // "name, phone, address" default *
-	WHERE           []string //"patient.id_patient = reservation.id_patient AND reservation.id_staff = '2'"
+	FROM_TABLES string //ej: "users,products" or: public.reservation, public.patient"
+	SELECT      string // "name, phone, address" default *
+
+	ID              string   // unique search (usado en indexdb)
 	SEARCH_ARGUMENT string   //"1,4,33"
-	ORDER_BY        string   // name,phone,address
-	SORT_DESC       bool     //default ASC
-	LIMIT           int      // 10, 5, 100. note: Postgres y MySQL: "LIMIT 10", SQLite: "LIMIT 10 OFFSET 0" OR "" no limit
-	RETURN_ANY      bool     // default string return []map[string]string, any = []map[string]interface{}
+	WHERE           []string //"patient.id_patient=reservation.id_patient, (AND) reservation.id_staff = '2'"
+
+	ORDER_BY  string // name,phone,address
+	SORT_DESC bool   //default ASC
+	LIMIT     int    // 10, 5, 100. note: Postgres y MySQL: "LIMIT 10", SQLite: "LIMIT 10 OFFSET 0" OR "" no limit
+
+	RETURN_ANY bool // default string return []map[string]string, any = []map[string]interface{}
+	RETURN_INT bool //retornar resultado de la consulta en la variable DataInt
 }
 
-type ReadResult struct {
-	DataString []map[string]string
-	DataAny    []map[string]any
-	Error      string
+type ReadResults struct {
+	ResultsString []map[string]string
+	ResultsAny    []map[string]any
+	DataInt       int
 }
 
 type DataBaseAdapter interface {
@@ -28,8 +32,8 @@ type DataBaseAdapter interface {
 	// items support in server db: []map[string]string, map[string]string
 	CreateObjectsInDB(table_name string, backup_required bool, items any) (err string)
 
-	ReadSyncDataDB(p ReadParams, data ...map[string]string) (result []map[string]string, err string)
-	ReadAsyncDataDB(p ReadParams, callback func(r ReadResult))
+	ReadSyncDataDB(p ReadParams) (r ReadResults, err string)
+	ReadAsyncDataDB(p ReadParams, callback func(r ReadResults, err string))
 
 	UpdateObjectsInDB(table_name string, data ...map[string]string) (err string)
 	DeleteObjectsInDB(table_name string, data ...map[string]string) (err string)
