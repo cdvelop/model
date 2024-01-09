e3c7ae944df3bf16b7b9220b49099bff16f6c15c
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2024-01-09 20:24:25 -0300

    add gamepad config

--- device.go
+++ device.go
@@ -2,8 +2,21 @@ package model
 
 type DevicePeripherals struct {
 	KeyboardClientAdapter
+	GamepadClientAdapter
 }
 
 type KeyboardClientAdapter interface {
 	KeyboardClientDisable(disable bool)
+}
+
+type GamepadClientAdapter interface {
+	//config button number and call function
+	GamepadCallFunRegisterButton(*GamepadConfig)
+}
+
+type GamepadConfig struct {
+	Connected      func()         //  al conectar gamepad
+	Disconnected   func()         //  al desconectar gamepad
+	ButtonAny      func()         //  al presionar cualquier botón
+	ButtonSpecific map[int]func() // función a ejecutar según numero de botón especifico
 }
