fa2de6de5db6fdbf31716075b7ce22b487e606b8
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2024-01-10 11:06:48 -0300

    refactor gamepadConfig

--- device.go
+++ device.go
@@ -11,12 +11,5 @@ type KeyboardClientAdapter interface {
 
 type GamepadClientAdapter interface {
 	//config button number and call function
-	GamepadCallFunRegisterButton(*GamepadConfig)
-}
-
-type GamepadConfig struct {
-	Connected      func()         //  al conectar gamepad
-	Disconnected   func()         //  al desconectar gamepad
-	ButtonAny      func()         //  al presionar cualquier botón
-	ButtonSpecific map[int]func() // función a ejecutar según numero de botón especifico
+	GamepadClientNotifyRegister(gamepadConfig any)
 }
