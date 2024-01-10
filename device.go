85893cfc81c2e01bfad5f39a514d033e29b64af3
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2024-01-10 11:21:38 -0300

    remove gamepad adapter

--- device.go
+++ device.go
@@ -2,14 +2,8 @@ package model
 
 type DevicePeripherals struct {
 	KeyboardClientAdapter
-	GamepadClientAdapter
 }
 
 type KeyboardClientAdapter interface {
 	KeyboardClientDisable(disable bool)
-}
-
-type GamepadClientAdapter interface {
-	//config button number and call function
-	GamepadClientNotifyRegister(gamepadConfig any)
 }
