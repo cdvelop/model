597bba7a75fce49bf2dc50201772434d3a8ce84c
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2024-01-10 12:49:14 -0300

    add GamepadClientAdapter interface in handlers

--- device.go
+++ device.go
@@ -2,8 +2,13 @@ package model
 
 type DevicePeripherals struct {
 	KeyboardClientAdapter
+	GamepadClientAdapter
 }
 
 type KeyboardClientAdapter interface {
 	KeyboardClientDisable(disable bool)
+}
+
+type GamepadClientAdapter interface {
+	GamepadRegister(notifyConnection, pressAnyButton, pressSpecificButtons any)
 }
